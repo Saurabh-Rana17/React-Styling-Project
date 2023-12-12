590baf92a2054f07f53e5e9244a3dca90138200b
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-12-12 21:08:24 +0530

    styled component section

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -14,18 +14,34 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({ $notvalid }) => ($notvalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({ $notvalid }) => ($notvalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $notvalid }) => ($notvalid ? "#ef4444" : "#374151")};
+  border-color: ${({ $notvalid }) => ($notvalid ? "#f73f3f" : "transparent")};
+  border: 1px solid;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
 `;
 
 export default function AuthInputs() {
@@ -52,21 +68,19 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className="para">
-          <Label className={`label ${emailNotValid && "invalid"}`}>Email</Label>
+          <Label $notvalid={emailNotValid}>Email</Label>
           <Input
             type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
-            // }}
-            className={emailNotValid ? "invalid" : undefined}
+            $notvalid={emailNotValid}
+            // className={emailNotValid ? "$notvalid" : ""}
             onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <Label>Password</Label>
+          <Label $notvalid={passwordNotValid}>Password</Label>
           <Input
             type="password"
-            className={passwordNotValid ? "invalid" : undefined}
+            $notvalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange("password", event.target.value)
             }
@@ -77,9 +91,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
